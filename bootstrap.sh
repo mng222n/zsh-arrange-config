56b528d83a2746d4e81542daffb1bdde45abd5d8
Minh Nguyen <anh.minh.anh.nguyen@gmail.com>
2021-07-10 00:22:52 +0700

    Create bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env zsh
+ZSHCONFIG=${HOME}/.zsh-arrange-config
+
+function zsh_bootstrap() {
+  echo "Cloning and installing zplugin"
+}
+
+function zsh_cleanup() {
+  echo "Cleaning up and releasing links to holding files"
+}
+
+zsh_bootstrap
